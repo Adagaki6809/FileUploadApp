a3ca0436da26a5ce7b6ee74abbdcd91e69505288
Adagaki6809 <include.ofstream.h@gmail.com>
2021-05-06 14:04:49 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using FileUploadApp.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FileUploadApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+        FilesContext _filesContext;
+        public HomeController(ApplicationContext context, FilesContext filesContext, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _filesContext = filesContext;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Files.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFiles(IFormFileCollection uploads)
+        {
+            foreach (var uploadedFile in uploads)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index2()
+        {
+            return View(_filesContext.People.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Create(PersonViewModel pvm)
+        {
+            Person person = new Person { Name = pvm.Name };
+            if (pvm.Avatar != null)
+            {
+                byte[] imageData = null;
+                // считываем переданный файл в массив байтов
+                using (var binaryReader = new BinaryReader(pvm.Avatar.OpenReadStream()))
+                {
+                    imageData = binaryReader.ReadBytes((int)pvm.Avatar.Length);
+                }
+                // установка массива байтов
+                person.Avatar = imageData;
+            }
+            _filesContext.People.Add(person);
+            _filesContext.SaveChanges();
+
+            return RedirectToAction("Index2");
+        }
+    }
+}
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FileUploadApp.Models
+{
+    public class PersonViewModel
+    {
+        public string Name { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
+
